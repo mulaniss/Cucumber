4c0058e440402a012bec079da71a38b6336c09a6
Dell <Dell@192.168.0.4>
2017-10-02 13:59:03 +0530

    First Commit

---
+++ Selenium_Cucumber_Project/src/com/selenium/cucumber/TestRunner.java
@@ -0,0 +1,11 @@
+package com.selenium.cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(features="Features")
+public class TestRunner {
+
+}
---
+++ Selenium_Cucumber_Project/src/com/selenium/stepdefinition/SmokeTest.java
@@ -0,0 +1,24 @@
+package com.selenium.stepdefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class SmokeTest {
+
+	@Given("^open firefox and start application$")
+	public void open_firefox_and_start_application() throws Throwable {
+
+	}
+
+	@When("^I enter valid username and valid password$")
+	public void I_enter_valid_username_and_valid_password() throws Throwable {
+
+	}
+
+	@Then("^User should be able to login successfully$")
+	public void User_should_be_able_to_login_successfully() throws Throwable {
+
+	}
+
+}
